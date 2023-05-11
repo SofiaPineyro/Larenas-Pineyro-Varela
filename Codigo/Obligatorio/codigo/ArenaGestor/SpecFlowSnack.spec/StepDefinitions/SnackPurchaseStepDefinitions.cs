5dab55edb952416351fa0dd397389483d0d72b63
FlorenciaV <florencia.v.peguri@gmail.com>
2023-05-11 17:29:47 -0300

    Create SnackPurchaseStepDefinitions.cs

---
+++ Codigo/Obligatorio/codigo/ArenaGestor/SpecFlowSnack.spec/StepDefinitions/SnackPurchaseStepDefinitions.cs
@@ -0,0 +1,81 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSnack.spec.StepDefinitions
+{
+    [Binding]
+    public class SnackPurchaseStepDefinitions
+    {
+        [Given(@"I am an spectator")]
+        public void GivenIAmAnSpectator()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"there are snacks available")]
+        public void GivenThereAreSnacksAvailable()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"the user double clicks the snack")]
+        public void WhenTheUserDoubleClicksTheSnack()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the snack subtotal is added to the tickets subtotal")]
+        public void ThenTheSnackSubtotalIsAddedToTheTicketsSubtotal()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the user completes the purchase with the snack added")]
+        public void ThenTheUserCompletesThePurchaseWithTheSnackAdded()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"there is one snack selected")]
+        public void GivenThereIsOneSnackSelected()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"the user selects another snack")]
+        public void WhenTheUserSelectsAnotherSnack()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the snack subtotal is added to the total")]
+        public void ThenTheSnackSubtotalIsAddedToTheTotal()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the user completes the purchase with the snacks added")]
+        public void ThenTheUserCompletesThePurchaseWithTheSnacksAdded()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the user did not select any snack")]
+        public void GivenTheUserDidNotSelectAnySnack()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"the user clicks Confirm")]
+        public void WhenTheUserClicksConfirm()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the user completes the purchase without any snack")]
+        public void ThenTheUserCompletesThePurchaseWithoutAnySnack()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
