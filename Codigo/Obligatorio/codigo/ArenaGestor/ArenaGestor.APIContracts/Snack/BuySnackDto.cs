6ee181d2c3dfc08b9b0a75e28a52890525ab8d3c
SofiaPineyro <86334236+SofiaPineyro@users.noreply.github.com>
2023-05-11 20:36:23 -0300

    Add snack purchase feature

---
+++ Codigo/Obligatorio/codigo/ArenaGestor/ArenaGestor.APIContracts/Snack/BuySnackDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ArenaGestor.APIContracts.Snack
+{
+	public class BuySnackDto
+	{
+		public BuySnackDto()
+		{
+		}
+
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
