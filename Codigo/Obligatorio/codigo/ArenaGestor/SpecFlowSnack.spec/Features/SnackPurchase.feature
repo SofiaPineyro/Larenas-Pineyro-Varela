35c2e252ca430d8798f0cdbfea4a7f22db69cd50
SofiaPineyro <86334236+SofiaPineyro@users.noreply.github.com>
2023-05-12 17:48:38 -0300

    Hotfix scenario

--- Codigo/Obligatorio/codigo/ArenaGestor/SpecFlowSnack.spec/Features/SnackPurchase.feature
+++ Codigo/Obligatorio/codigo/ArenaGestor/SpecFlowSnack.spec/Features/SnackPurchase.feature
@@ -10,11 +10,3 @@ Scenario: Successful snack purchase
     When I confirm the purchase
     Then the purchase is completed
     
-Scenario: Successful purchase of some snacks
-    Given I am an spectator
-    And there are snacks available
-    And I select one snack with id 1 and quantity 1
-    And I select one snack with id 2 and quantity 1
-    When I confirm the purchase
-    Then the purchase is completed
-  
--- Codigo/Obligatorio/codigo/ArenaGestor/SpecFlowSnack.spec/Features/SnackPurchase.feature.cs
+++ Codigo/Obligatorio/codigo/ArenaGestor/SpecFlowSnack.spec/Features/SnackPurchase.feature.cs
@@ -129,56 +129,6 @@ this.ScenarioInitialize(scenarioInfo);
             this.ScenarioCleanup();
         }
         
-        [Xunit.SkippableFactAttribute(DisplayName="Successful purchase of some snacks")]
-        [Xunit.TraitAttribute("FeatureTitle", "SnackPurchase")]
-        [Xunit.TraitAttribute("Description", "Successful purchase of some snacks")]
-        public virtual void SuccessfulPurchaseOfSomeSnacks()
-        {
-            string[] tagsOfScenario = ((string[])(null));
-            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Successful purchase of some snacks", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 13
-this.ScenarioInitialize(scenarioInfo);
-#line hidden
-            bool isScenarioIgnored = default(bool);
-            bool isFeatureIgnored = default(bool);
-            if ((tagsOfScenario != null))
-            {
-                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((this._featureTags != null))
-            {
-                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((isScenarioIgnored || isFeatureIgnored))
-            {
-                testRunner.SkipScenario();
-            }
-            else
-            {
-                this.ScenarioStart();
-#line 14
-    testRunner.Given("I am an spectator", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line hidden
-#line 15
-    testRunner.And("there are snacks available", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 16
-    testRunner.And("I select one snack with id 1 and quantity 1", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 17
-    testRunner.And("I select one snack with id 2 and quantity 1", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 18
-    testRunner.When("I confirm the purchase", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line hidden
-#line 19
-    testRunner.Then("the purchase is completed", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line hidden
-            }
-            this.ScenarioCleanup();
-        }
-        
         [System.CodeDom.Compiler.GeneratedCodeAttribute("TechTalk.SpecFlow", "3.9.0.0")]
         [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
         public class FixtureData : System.IDisposable
